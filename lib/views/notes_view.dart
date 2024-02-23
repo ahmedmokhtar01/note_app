14964f9f8ff3eadaa1fca19573423c9a2bcfbefd
ahmed gamal moukhtar <ahmedgamalmoukhtar@ahmeds-MacBook-Pro.local>
2024-02-23 18:27:53 +0200

    add floating action button to display the Modal sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/Notes_view_body.dart';
+import 'package:note_app/views/modal_sheet_content.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -7,6 +8,18 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(width: 500, child: ModalSheetContent());
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
       body: NotesViewBody(),
     );
   }
